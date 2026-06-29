5dae6b4ed2578d4592ade088e980b4d515c9eb78
roenstore <thomasmugiprasetyo@gmail.com>
2026-06-30 06:48:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const search=document.getElementById("search");
+const search = document.getElementById("search");
+
 
 search.addEventListener("keyup",()=>{
 
-let value=search.value.toLowerCase();
+let value = search.value.toLowerCase();
 
-let cards=document.querySelectorAll(".card");
+let cards = document.querySelectorAll(".card");
+
 
 cards.forEach(card=>{
 
-let title=card.querySelector("h2").innerText.toLowerCase();
+let title = card.querySelector("h2").innerText.toLowerCase();
+
 
 if(title.includes(value)){
 
@@ -23,3 +26,37 @@ card.style.display="none";
 });
 
 });
+
+
+
+
+
+let keranjang = [];
+
+
+
+function tambahKeranjang(nama,harga){
+
+
+keranjang.push({
+
+nama:nama,
+harga:harga
+
+});
+
+
+
+document.getElementById("jumlah").innerText = keranjang.length;
+
+
+
+alert(nama+" masuk keranjang");
+
+
+
+console.log(keranjang);
+
+
+
+}
