dd8856277da960a7ffbdcd7669560d9015ebb74b
roenstore <thomasmugiprasetyo@gmail.com>
2026-06-30 08:17:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,35 @@ sidebar.classList.toggle("active");
 overlay.classList.toggle("active");
 
 }
+
+
+// tutup sidebar kalau klik luar
+
+document.addEventListener("click",function(e){
+
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const icon = document.querySelector(".menuIcon");
+
+
+if(
+sidebar &&
+sidebar.classList.contains("active") &&
+!sidebar.contains(e.target) &&
+!icon.contains(e.target)
+){
+
+sidebar.classList.remove("active");
+overlay.classList.remove("active");
+
+}
+
+});
+
+
+
+// SEARCH
+
 const search = document.getElementById("search");
 
 if(search){
@@ -17,22 +46,26 @@ let value = search.value.toLowerCase();
 
 let cards = document.querySelectorAll(".card");
 
+
 cards.forEach(card=>{
 
 let title = card.querySelector("h2").innerText.toLowerCase();
 
+
 if(title.includes(value)){
-card.style.display="block";
+
+card.style.display="flex";
+
 }else{
+
 card.style.display="none";
-}
-
-});
-
-});
 
 }
 
 
+});
 
-  
+
+});
+
+}
